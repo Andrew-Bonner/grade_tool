75bf7be9866137313c410ff753431618e438bfd6
Andrew Bonner <andrew.bonner@bsu.edu>
2023-11-15 17:41:54 -0500

    make a failing test to in first step of TDD and Red-Green-Refactor

--- test/grade_tool_test.dart
+++ test/grade_tool_test.dart
@@ -1,1 +1,10 @@
-void main() {}
+import 'package:test/test.dart';
+
+void main() {
+  test(('100 is an A'), () {
+    int percent = 100;
+    final converter = GradeConverter();
+    String letter = converter.changePercentToLetter(percent);
+    expect(letter, 'A');
+  });
+}
