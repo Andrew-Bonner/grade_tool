cc049a266eaec662523404d8293b3295b674e1ba
Andrew Bonner <andrew.bonner@bsu.edu>
2023-11-15 17:58:20 -0500

    create another failing test case

--- test/grade_tool_test.dart
+++ test/grade_tool_test.dart
@@ -8,4 +8,10 @@ void main() {
     String letter = gradeTool.convertPercentToLetter(percent);
     expect(letter, 'A');
   });
+  test(('89.5 is an A'), () {
+    double percent = 89.5;
+    final gradeTool = GradeConverter();
+    String letter = gradeTool.convertPercentToLetter(percent);
+    expect(letter, 'B');
+  });
 }
